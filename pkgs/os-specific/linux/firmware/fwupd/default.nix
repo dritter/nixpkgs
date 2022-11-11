7663fc2eae570321355ddad1359538adea5a7e7f
Dominik Ritter <dritter03@googlemail.com>
2022-11-11 23:38:46 +0100

    fwupd: 1.8.4 -> 1.8.7

--- pkgs/os-specific/linux/firmware/fwupd/default.nix
+++ pkgs/os-specific/linux/firmware/fwupd/default.nix
@@ -114,7 +114,7 @@ let
 
   self = stdenv.mkDerivation rec {
     pname = "fwupd";
-    version = "1.8.4";
+    version = "1.8.7";
 
     # libfwupd goes to lib
     # daemon, plug-ins and libfwupdplugin go to out
@@ -123,7 +123,7 @@ let
 
     src = fetchurl {
       url = "https://people.freedesktop.org/~hughsient/releases/fwupd-${version}.tar.xz";
-      sha256 = "sha256-rfoHQ0zcKexBxA/vRg6Nlwlj/gx+hJ3sfzkyrbFh+IY=";
+      sha256 = "sha256-8qUJotM1A4QujPIjsxKPGu64oqUQcMAKAAGCmybkXPY=";
     };
 
     patches = [
